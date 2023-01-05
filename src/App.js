5736090d66e1bbcaf1e5954a1a7cf17e247978ec
GRB.github <76528190+GRBgithub@users.noreply.github.com>
2023-01-05 10:14:31 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Test from "./Test";
+
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Element>
+      <div className="App">
+        <Test></Test>
+      </div>
+    </Element>
   );
 }
+
+const Element = styled.div`
+  .App {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
 
 export default App;
